92f84700a59ba6279c0e030fa7cc734fe5157a31
Andres Felipe Vargas <pipevarg1230@gmail.com>
2022-06-26 08:56:04 -0500

    v

---
+++ Parcial/Facturacion.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+
+namespace Parcial
+{
+    internal class Facturacion
+    {
+
+
+        double Subtotal(double[] precios, int[] unidades)
+        {
+            double subtotal = 0;
+
+            for (int i = 0; i < precios.Length; i++)
+            {
+                subtotal += precios[i] * unidades[i];
+            }
+
+            return subtotal;
+        }
+
+        double Iva(double a)
+        {
+            return a * 0.19;
+        }
+
+        double Total(double subtotal, double iva)
+        {
+            return subtotal + iva;
+        }
+
+        double Descuento(double total)
+        {
+            double descuento = 0;
+            if (total >= 500000)
+            {
+                descuento = total * 0.1;
+            }
+
+            return descuento;
+        }
+
+        double Neto(double total, double descuento)
+        {
+            return total - descuento;
+        }
+
+        public void Calcular(double[] prec,int[] unit)
+        {
+            double sub = Subtotal(prec,unit);
+            double iva = Iva(sub);
+            double total = Total(sub,iva);
+            double descuento = Descuento(total);
+            double neto = Neto(total, descuento);
+
+            Write($"Subtotal:{sub}\n" +
+                  $"Iva:{iva}\n" +
+                  $"Total Factura:{total}\n" +
+                  $"Descuento:{descuento}\n" +
+                  $"Neto:{neto}");
+        }
+
+
+
+        
+
+    }
+}
--- Parcial/Program.cs
+++ Parcial/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using static System.Console;
 
 namespace Parcial
 {
@@ -9,6 +10,123 @@ namespace Parcial
     {
         static void Main(string[] args)
         {
+            //declaramos nuestras variables
+
+            //logicas
+            bool ejecutar = true;
+            int opcion = 0;
+            bool imprimir = true;
+            List<string> list_nombres= new List<string>();
+            int productos;
+
+
+            //pedir el numero de productos del catalago
+
+            Write("Cuantos productos se venden .:");
+            while(!int.TryParse(ReadLine(),out productos))
+            {
+                Write("Ingrese un precio valido, recuerde no usar letras ni caracteres especiales\n.:");
+            }
+
+            //arreglo de nombres |1.Jean | 2.Tenis | 3.Blusas | 
+            string[] nombres = new string[productos];
+
+
+            //arreglo de precios |1.Jean | 2.Tenis | 3.Blusas | 
+            double[] precios= new double[productos];
+
+            //arreglo de unidades|1.Jean | 2.Tenis | 3.Blusas | 
+            int[] unidades = new int[productos];
+
+
+            for (int i = 0; i < productos; i++)
+            {
+                Write($"Ingrese el nombre del producto{i + 1} .:");
+                list_nombres.Add(ReadLine());
+            }
+
+            nombres = list_nombres.ToArray();
+
+            for (int i = 0; i < nombres.Length; i++)
+            {
+                Write($"Ingrese el valor de l@s {nombres[i]} .:");
+                while (!double.TryParse(ReadLine(), out precios[i]))
+                {
+                    Write("Ingrese un precio valido, recuerde no usar letras ni caracteres especiales\n.:");
+                }
+            }
+
+
+            // Asignamos los precios unitarios de cada producto
+            do
+            {
+                if (imprimir)
+                {
+                    Clear();
+                    MenuVentas(nombres);
+                    opcion = int.Parse(ReadLine());
+                }
+                else
+                {
+                    Clear();
+                    Write("!!!! Ingrese una opcion adecuada!!!!");
+                    MenuVentas(nombres);
+                    opcion = int.Parse(ReadLine());
+                }
+
+                if (opcion>nombres.Length || opcion < 1)
+                {
+                    imprimir = false;
+                }
+                else
+                {
+                    Write($"Cuant@s {nombres[opcion - 1]} se vendieron .:");
+                    unidades[opcion - 1] += int.Parse(ReadLine());
+                }
+                
+                if (opcion==nombres.Length+1)
+                {
+                    ejecutar = false;
+                }
+               
+
+                
+
+                
+
+            } while (ejecutar);
+
+
+
+            WriteLine("Num.--------Descripcion\t\tvlr Unit\tCantidad\tVlr Total");
+            for (int i = 0; i < nombres.Length; i++)
+            {
+                WriteLine($"{i+1}.----------{nombres[i]}\t\t{precios[i]}\t\t{unidades[i]}\t\t{precios[i] * unidades[i]}");
+            }
+
+            Write("\n");
+
+            new Facturacion().Calcular(precios,unidades);
+
+
+
+            //Fin del programa 
+
+            ReadKey();
+        }
+
+        //Funciones 
+        private static void MenuVentas(string[] nombres)
+        {
+            
+            WriteLine("Que producto se vendio");
+            for (int i = 0; i < nombres.Length; i++)
+            {
+                WriteLine($"{i + 1}.{nombres[i]}");
+            }
+            WriteLine($"{nombres.Length + 1}.Facturacion");
+            Write("opcion .:");
+
         }
     }
 }
